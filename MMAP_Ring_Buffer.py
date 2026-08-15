aa7d1306e4d2ee0429e07abdcd1948ac269b7ae1
Nihal Shikalgar <nihalshikalgar07@gmail.com>
2026-08-15 11:12:58 +0530

    MMAP_Ring_Buffer

--- MMAP_Ring_Buffer.py
+++ MMAP_Ring_Buffer.py
@@ -12,27 +12,45 @@ BUFFER_SIZE = 1024     #number of orders in ring buffer 1024 slots
 HEADER_SIZE = 16       #write pos + read pos, allocated 16 bytes, write pos =8 read pos = 8
 OREDER_SIZE = 24       #24 bytes
 
+"""calculating total size of mmap file"""
 FILESIZE = HEADER_SIZE + (BUFFER_SIZE * OREDER_SIZE)
 
-ORDER_FORMAT = struct.Struct("QdQ")
+ORDER_FORMAT = struct.Struct("QdQ")    #arrangement of order i.e. order id, price, quantity
+#Q -> 8 byte unsigned int
+#d -> 8 byte float
 
-HEADER_FORMAT = struct.Struct("QQ")
+HEADER_FORMAT = struct.Struct("QQ")   #binary layout of ring buffer
+#contains 2 values Q -> writepos (8 byt), Q -> readpos(8 bytes)
+
+
 
 class MMapRingBuffer:
     
-    def __init__(self, filename = "orders.mmap"):
+    def __init__(self, filename = "orders.mmap"):   #init constructor with self parametr and default filename
         self.filename = filename
         
-        self.fd = os.open(filename, os.O_RDWR | os.O_CREAT)
+        self.fd = os.open(filename, os.O_RDWR | os.O_CREAT)  #create a file
+        #self.fd -> file descriptor OS provided identifier for the opened file
+        #os.O_RDWR -> open file read and write
+        #os.O_CREAT -> create the file
         
-        os.ftruncate(self.fd, FILESIZE)
+        os.ftruncate(self.fd, FILESIZE)    #set file size
         
+        """mmap object creation"""
         self.mm = mmap.mmap(self.fd, FILESIZE, access=mmap.ACCESS_WRITE)
-        
+        #self.mm - stores the object returned by mmap.mmap()
+        #mmap.mmap -> map file in memory
+
         write_pos, read_pos = HEADER_FORMAT.unpack_from(self.mm,0)
+        #unpack_from -> reads binary data
+        #self.mm -> mmap memory region
+        #0 -> starting from 0 byte
         
         if write_pos >= BUFFER_SIZE or read_pos >= BUFFER_SIZE:
             HEADER_FORMAT.pack_into(self.mm, 0, 0, 0)
+            #pack_into -> take data,convert into binary bytes, write into mmap memory
+            #0 -> starting byte position
+            #0,0 -> write pos, read pos 
             
     
     def getwritepos(self):
